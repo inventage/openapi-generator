6065a56538fef3e154a8026bc56e309e48f1399d
Tobias Gmuender <tobias.gmuender@inventage.com>
2018-12-07 09:32:18 +0100

    Add utility to set version

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/InventageJavaServerCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/InventageJavaServerCodegen.java
@@ -515,12 +515,6 @@ public class InventageJavaServerCodegen extends AbstractJavaJAXRSServerCodegen {
         return false;
     }
 
-    /** {@inheritDoc} */
-    @Override
-    public String toBooleanGetter(final String name) {
-        return super.toGetter(name);
-    }
-
     private enum NamingStrategy {
         AUTO,
         PATH;
